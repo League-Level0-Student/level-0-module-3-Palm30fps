63ab69209912696c63265e1c8658094e4e1ec4fc
League Student <league@iMac-14.attlocal.net>
2020-01-12 14:55:12 -0800

    Update SpudCounter.java
    Finished module 3

--- src/_99_extra/SpudCounter.java
+++ src/_99_extra/SpudCounter.java
@@ -1,53 +1,23 @@
 package _99_extra;
 
 public class SpudCounter {
-public static void main(String[] args) {
+	public static void main(String[] args) {
+
+		for (int i = 1; i < 9; i++) {
+
+			if (i != 4) {
+				System.out.println(i + " potato");
+			}
+		
+if( i== 4) {System.out.println(4);
+}	
+
+if(i==8) {System.out.println("more");		
+		
+		
+		}
 	
-for (int i = 1; i < 6; i++) {
 	
-
-	if(i!4)
-System.out.println(i+" potato");}
-
-
-
-
-
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
-
